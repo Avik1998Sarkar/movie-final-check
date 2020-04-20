a199aaa05753f5857892a10b315f4acffdddc3ce
Avik1998Sarkar <36466256+Avik1998Sarkar@users.noreply.github.com>
2020-04-20 10:14:59 +0530

    Update script.js

--- movie-ui/js/script.js
+++ movie-ui/js/script.js
@@ -23,3 +23,23 @@ $('#main-nav a').on('click', function (e) {
 });
 
 $('[data-toggle="tooltip"]').tooltip();
+
+
+const searchFun = () => {
+	let filter = document.getElementById('searchBar').value.toUpperCase();
+	let col = document.getElementById('movie-section');
+	let eachCard = col.getElementsByClassName('mt-2');
+
+	for (var i = 0; i < eachCard.length; i++) {
+		let name = eachCard[i].getElementsByTagName('h6')[0];
+
+		if (name) {
+			let textValue = name.innerText;
+			if (textValue.toUpperCase().indexOf(filter) > -1) {
+				eachCard[i].style.display = "";
+			} else {
+				eachCard[i].style.display = "none";
+			}
+		}
+	}
+}
